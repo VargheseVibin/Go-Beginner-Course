6be4f89d5009a96c710899f0f0114ce918a37a55
Vibin Varghese <vibinvvarghese@gmail.com>
2022-01-29 23:15:46 +0530

    More Web-App Iterations

---
+++ 22-Web-App-Template-App-Wide-Config/cmd/web/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/VargheseVibin/GoWebPackage01/pkg/config"
+	"github.com/VargheseVibin/GoWebPackage01/pkg/handlers"
+	"github.com/VargheseVibin/GoWebPackage01/pkg/render"
+)
+
+const portNumber = ":8081"
+
+func main() {
+	var appConf config.AppConfig
+	var err error
+	appConf.TemplateCache, err = render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+	appConf.UseCache = false
+	repo := handlers.NewRepo(&appConf)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&appConf)
+
+	fmt.Printf("This is a sample line")
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Printf("Starting Application on Port %v", portNumber)
+
+	http.ListenAndServe(portNumber, nil)
+}
---
+++ 22-Web-App-Template-App-Wide-Config/pkg/render/render.go
@@ -0,0 +1,72 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/VargheseVibin/GoWebPackage01/pkg/config"
+)
+
+var functions = template.FuncMap{}
+var appConf *config.AppConfig
+
+func NewTemplates(a *config.AppConfig) {
+	appConf = a
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	var templCache map[string]*template.Template
+	if appConf.UseCache {
+		templCache = appConf.TemplateCache
+	} else {
+		templCache, _ = CreateTemplateCache()
+	}
+
+	templateForRender, ok := templCache[tmpl]
+	if !ok {
+		log.Fatal("No Template " + tmpl + " found in the TemplateCache")
+	}
+	// Here byteBuffer is created to read-in the template into the buffer.
+	// Unlike, in the last project, where the template was directly read from the disk
+	byteBuffer := new(bytes.Buffer)
+	_ = templateForRender.Execute(byteBuffer, nil)
+	_, err := byteBuffer.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error wiritng template to the browser", err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		baseName := filepath.Base(page)
+		fmt.Println("Render looking for page", baseName)
+		ts, err := template.New(baseName).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		layoutMatches, err1 := filepath.Glob("./templates/*.layout.gohtml")
+		if err1 != nil {
+			return myCache, err1
+		}
+		if len(layoutMatches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[baseName] = ts
+	}
+	return myCache, nil
+
+}
